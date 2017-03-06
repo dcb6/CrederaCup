ff5eb07bdd845124407927b8878247a66a1534e7
dcb6 <dcb6@rice.edu>
2017-03-06 11:11:14 -0600

    Brute force not working

--- client.py
+++ client.py
@@ -19,30 +19,8 @@ def get_status(code):
 
 def on_message(ws, msg):
     data = json.loads(msg)
-    print data
-
-    # options:
-
-    # initialize global variables
-    global retire_early
-    global graph
-    global has_changed_tire
-    global i
-
-    ## FIRST RUN
-    # initial weather
-    if "weather" in data:
-        result = data["weather"]
-        temp = result["temp"]
-        is_raining = result["isRaining"]
-
-    elif "trackParams" in data:
-        results = data["trackParams"]
-        num_turns = results["numTurns"]
-        num_laps = results["numLaps"]
-
     ## EACH LAP
-    elif "lapResult" in data:
+    if "lapResult" in data:
         result = data["lapResult"]
         lap_number = result["lapNumber"]
         lap_time = result["lapTime"]
@@ -50,38 +28,40 @@ def on_message(ws, msg):
         made_pit_stop = result["madePitStop"]
         tire = result["tire"]
 
-        ## Data collection
-        graph[i] = lap_time
+        global retire_early
+        global graph
+        global has_changed_tire
+        global i
 
-        ## Determine whether or not to make a pit stop
-        need_fuel = fuel < 2
-        need_tire = tire < 5
-        make_pit_stop = need_fuel or need_tire
-        if retire_early:
-            response = json.dumps({"instruction":"retire"})
-        elif make_pit_stop:
-            pit_params = { "instruction": "pit"}
-            pit_params["spoilerAngle"] = 5.0 # [0.0, 10.0], default=5.0
-            pit_params["camberAngle"] = 2.5 # [0.0, 5.0], default=2.5
-            pit_params["airIntakeDiameter"] = 6.0 # # airIntakeDiameter [4.0, 8.0], default=6.0
-            pit_params["fuel"] = "C" # A,B,C
-            pit_params["tire"] = "A" # A,B,C,D
+        global best_time
+        global best_fuel
+        global best_tire
+        global best_spoiler
+        global best_camber
+        global best_air_intake
 
-            # if need_fuel:
-            #     pit_params["fuel"] = "A" # A,B,C,D
-            # if need_tire < 5:
-            #     pit_params["tire"] = "A" # A,B,C
+        global fuel
+        global tire
+        global spoiler
+        global camber
+        global intake
 
-            response = json.dumps(pit_params)
-        else:
-            response = json.dumps({"instruction":"continue"})
+        if lap_time < best_time:
+            best_time = lap_time
+            best_fuel = fuel
+            best_tire = tire
+            best_spoiler = spoiler
+            best_camber = camber
+            best_air_intake = air_intake
 
+        response = json.dumps({"instruction":"retire"})
         ws.send(response)
+
     elif "raceResult" in data:
-        print "Race complete!"
-        print "Level {}".format(data["raceResult"]["circuit"])
-        print "Status: {}".format(get_status(data["raceResult"]["status"]))
-        print "Total time: {}".format(data["raceResult"]["totalTime"])
+        # print "Race complete!"
+        # print "Level {}".format(data["raceResult"]["circuit"])
+        # print "Status: {}".format(get_status(data["raceResult"]["status"]))
+        # print "Total time: {}".format(data["raceResult"]["totalTime"])
         ws.close()
 
 def on_error(ws, err):
@@ -91,52 +71,40 @@ def on_close(ws):
     print "Connection closed."
 
 def on_open(ws):
-    print "Connection established."
-    global race
-    if race == "practice":
-        # set weather
-        weather = {"instruction": "setWeather"}
-        weather["temp"] = 80 # [70, 80]
-        weather["isRaining"] = False # True or False
-        response = json.dumps(weather)
-        ws.send(response)
+    global fuel
+    global tire
+    global spoiler
+    global camber
+    global intake
 
-    # get weather
-    response = json.dumps({"instruction": "getWeather"})
-    ws.send(response)
-    # get vehicle parameters
-    # response = json.dumps({"instruction": "getVehicleParams"})
-    # ws.send(response)
-    # set vehicle parameters
+    print "Connection established."
+
     vehicle_params = {"instruction": "setVehicleParams"}
-    vehicle_params["fuel"] = "A" # A,B,C,D
-    vehicle_params["tire"] = "A" # A,B,C
-    vehicle_params["spoilerAngle"] = 5.0 # [0.0, 10.0], default=5.0
-    vehicle_params["camberAngle"] = 2.5 # [0.0, 5.0], default=2.5
-    vehicle_params["airIntakeDiameter"] = i # [4.0, 8.0], default=6.0
+    vehicle_params["fuel"] = fuel # A,B,C,D
+    vehicle_params["tire"] = tire # A,B,C
+    vehicle_params["spoilerAngle"] = spoiler # [0.0, 10.0], default=5.0
+    vehicle_params["camberAngle"] = camber # [0.0, 5.0], default=2.5
+    vehicle_params["airIntakeDiameter"] = intake # [4.0, 8.0], default=6.0
 
     response = json.dumps(vehicle_params)
     ws.send(response)
-    # get track parameters
-    response = json.dumps({"instruction": "getTrackParams"})
-    ws.send(response)
+
     # start race
     response = json.dumps({ "instruction": "beginRace" })
     ws.send(response)
 
-
-## OPTIONS
+#
+# ## OPTIONS
 race = "practice"
-retire_early = True
-loop_start = 4
-loop_end = 8
-loop_iter = 1
-x_title = 'air intake diameter'
-y_title = 'lap time'
-
-# globals
-graph = {}
-
+#
+# # globals
+# best_time = float('inf')
+#
+# best_fuel = 0
+# best_tire = 0
+# best_spoiler = 0
+# best_camber = 0
+# best_air_intake = 0
 
 # url being selected
 url_options = {"practice":"wss://play.crederacup.com/season/I/practice",
@@ -146,29 +114,46 @@ url_options = {"practice":"wss://play.crederacup.com/season/I/practice",
                "italian":"wss://play.crederacup.com/season/I/race/ITALIANGRANDPRIX",
                "monaco":"wss://play.crederacup.com/season/I/race/MONACOGRANDPRIX"
                 }
-url = url_options['practice']
 
-for i in range(loop_start,loop_end + loop_iter,loop_iter):
-    print 'iterated value:', i
-    if __name__ == "__main__":
-        print "Attempting websocket connection to {}".format(url)
+url = url_options[race]
 
-        ws = websocket.WebSocketApp(url,
-                            header = ["X-Credera-Auth-Token: " + "5c8a27d7-513d-4801-a1bb-b8b4b87a6e43"],
-                            on_message = on_message,
-                            on_error = on_error,
-                            on_close = on_close)
+ws = websocket.WebSocketApp(url,
+                    header = ["X-Credera-Auth-Token: " + "5dd8c4eb-32f5-4d09-8db3-652feeac4f61"],
+                    on_message = on_message,
+                    on_error = on_error,
+                    on_close = on_close)
 
-        # global variables
-        has_changed_tire = False
+ws.on_open = on_open
+ws.run_forever()
 
-        ws.on_open = on_open
-        ws.run_forever()
-        print 'trying to iterate'
+sys.exit()
 
-print 'graph:', graph
-# cchelper.plot_lines([graph], y_title + ' vs ' + x_title, x_title, y_title)
-# cchelper.show()
+for fuel in ['A','B','C','D']:
+    for tire in ['A','B','C']:
+        for spoiler in range(0, 11, 5):
+            for camber in range(0, 6, 5):
+                for intake in range(4, 9, 2):
+                    if __name__ == "__main__":
+                        print "Attempting websocket connection to {}".format(url)
+
+                        ws = websocket.WebSocketApp(url,
+                                            header = ["X-Credera-Auth-Token: " + "0b9e130c-2ee9-4477-8a1a-6a68df512bdb"],
+                                            on_message = on_message,
+                                            on_error = on_error,
+                                            on_close = on_close)
+
+                        ws.on_open = on_open
+                        ws.run_forever()
+                        print 'trying to iterate'
+
+print 'best_tire: ', best_tire
+print 'best_camber: ', best_camber
+print 'best_fuel: ', best_fuel
+print 'best_spoiler: ', best_spoiler
+print 'best_air_intake: ', best_air_intake
+print 'best_time: ', best_time
+
+
 
     # loop attempt
     # for i in range(1, 3, 1):
